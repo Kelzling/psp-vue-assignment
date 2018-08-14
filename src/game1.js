19fdab38ace1e8723a304fd68a81e30de8c8e023
Kelsey <31727098+Kelzling@users.noreply.github.com>
2018-08-14 14:10:10 +1200

    Added Vue components to frontend
    Components for Instructions and Game Buttons.

--- src/game1.js
+++ src/game1.js
@@ -5,6 +5,11 @@ function main () { // eslint-disable-line no-unused-vars
       props: ['instructions'],
       template: '<div><h2>Instructions</h2> {{ instructions }}</div>'
     })
+    
+  Vue.component('game-btn', {
+    props: ['newButton', 'method'],
+    template: '<input type="button" class="gameBtn" v-bind:value="newButton.value" @click="method">'
+  }) // couldn't figure out how to bind v-if successfully within a template at this stage
 
   var game1 = new Vue({ // eslint-disable-line no-unused-vars
     el: '#game-container',
@@ -18,10 +23,9 @@ function main () { // eslint-disable-line no-unused-vars
       history: '',
       placeholder: 'Enter your guess',
       // for hiding buttons
-      startSeen: true,
-      submitSeen: false,
-      restartSeen: false,
+      btnSeen: 'start',
       // variables
+      buttons: [{value: 'Start Game'}, {value: 'Submit Guess'}, {value: 'Restart Game?'}],
       userInput: '',
       userGuess: 0,
       guessCount: 0,
@@ -36,9 +40,7 @@ function main () { // eslint-disable-line no-unused-vars
         this.guessCount = 0
         this.number = Math.round(Math.random() * 100)
         // hide undesired buttons
-        this.startSeen = false
-        this.restartSeen = false
-        this.submitSeen = true
+        this.btnSeen = 'submit'
       },
       checkInput: function () {
         this.userGuess = Number(this.userInput)
@@ -65,8 +67,7 @@ function main () { // eslint-disable-line no-unused-vars
           } else if (this.userGuess === this.number) {
             // this one would show 'undefined' instead of guessCount if I put it in the responseOptions array... might be worth investigating.
             this.response = `You got it in ${this.guessCount} trials!`
-            this.submitSeen = false
-            this.restartSeen = true
+            this.btnSeen = 'restart'
           }
         } else {
           this.response = this.responseOptions[2]
