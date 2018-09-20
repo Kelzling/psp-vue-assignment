ce88bc993e31d73d8c4ee54e4b8947a9173d892c
Kelsey Vavasour <kelzling@gmail.com>
2018-09-20 21:35:04 +1200

    Refactored Controller, Added Game Swapping Controller now consists of only one Vue object to enable easy swapping between games using v-if Initially looked into nesting my existing Vue objects within another Vue object that would just control the games but my research suggested this was a bad idea.

--- src/main.js
+++ src/main.js
@@ -11,12 +11,91 @@ Vue.component('game-instructions', {
 
 Vue.component('game-btn', {
   props: ['newButton', 'method'],
-  template: '<input type="button" class="gameBtn" v-bind:value="newButton.value" @click="method">'
+  template: '<input type="button" class="gameBtn" v-bind:value="newButton" @click="method">'
 }) // couldn't figure out how to bind v-if successfully within a template at this stage
+
+Vue.component('version-btn', {
+  props: ['gameNum', 'method'],
+  template: '<input type="button" class="versionBtn" id="gameNum" v-bind:value="gameNum" @click="method">'
+})
 
 // main function to be run after the initial HTML has loaded
 function main () { // eslint-disable-line no-unused-vars
-  var game1 = new Vue({ // eslint-disable-line no-unused-vars
+  var controller = new Vue({ // eslint-disable-line no-unused-vars
+    el: '#container',
+    data: {
+      placeholder: 'Enter your guess',
+      instructions: 'Welcome to the Number Guessing Games. Please select a number above to play a game.',
+      gameSeen: 'splash',
+      btnSeen: 'Start Game',
+      validButtons: ['Start Game', 'User Response', 'Restart Game?'],
+      buttons: [],
+      history: [],
+      userInput: '',
+      response: '',
+      allMyBrains: [new ThinkerBrain(), new HotColdThinkerBrain(), new GuesserBrain(), new HotColdGuesserBrain()],
+      currentBrain: null
+    },
+    methods: {
+      swapGame: function (gameNum) {
+        this.currentBrain = this.allMyBrains[gameNum - 1]
+        this.instructions = this.currentBrain.instructions
+        this.buttons = this.currentBrain.buttons
+        this.history = []
+        this.response = ''
+        this.userInput = ''
+        this.swapButton('Start Game')
+        this.gameSeen = `game-${gameNum}`
+        // update buttons to denote currently active game
+      },
+      swapButton: function (newBtn) {
+        if (this.validButtons.includes(newBtn)) {
+          this.btnSeen = newBtn
+        } else {
+          console.warn('That button is not a valid option')
+        }
+      },
+      startThinkerGame: function () {
+        this.history = []
+        this.userInput = ''
+        this.swapButton('User Response')
+        this.currentBrain.startGame()
+      },
+      startGuesserGame: function () {
+        // reinitialise variables
+        this.history = []
+        this.response = ''
+        // display user buttons and get the first guess
+        let firstGuess = this.currentBrain.startGame()
+        this.response = `Hmm, I guess ${firstGuess}.`
+        this.swapButton('User Response')
+      },
+      userGuessHandler: function () {
+        if (this.userInput) {
+          // only runs if the user has entered something into the user input box
+          let brainResponse = this.currentBrain.turnHandler(this.userInput)
+          this.userInput = ''
+          this.history = this.currentBrain.formattedHistory
+          if (brainResponse.gameState === 'finished') {
+            this.swapButton('Restart Game?')
+          }
+        }
+      },
+      userResponseHandler: function (message) {
+        // add the previous guess and the users response to the history
+        let brainResponse = this.currentBrain.turnHandler(message)
+        this.history = this.currentBrain.formattedHistory
+        if (brainResponse.gameState === 'finished') {
+          this.response = brainResponse.response
+          this.swapButton('Restart Game?')
+        } else {
+          this.response = `Ok then, I'll guess ${brainResponse.response}.`
+        }
+      }
+    }
+  })
+
+  /* var game1 = new Vue({ // eslint-disable-line no-unused-vars
     el: '#game-container',
     data: {
       // messages
@@ -183,5 +262,5 @@ function main () { // eslint-disable-line no-unused-vars
         }
       }
     }
-  })
+  }) */
 }
